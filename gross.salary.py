8e492e7eb4710c631d782ffac530f665834bfc55
aashlesha76 <60506126+aashlesha76@users.noreply.github.com>
2020-04-23 13:02:28 +0530

    Add files via upload
    calculate the  gross salary

---
+++ gross.salary.py
@@ -0,0 +1,11 @@
+bs=float(input("Enter your basic salary :\n"))
+da=bs*0.6
+hra=bs*0.15
+gs=bs+da+hra
+print("Your Gross Salary is: ",gs)# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
